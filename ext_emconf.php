1f6192d9ec534e3929c781997740dc57cadac32f
Arno Schoon <arno@maxserv.com>
2015-07-31 16:50:18 +0200

    [DII-719] - Bare Composer manifest to bootstrap dependencies.

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'S3 driver for FAL',
+	'description' => 'Connect FAL/TYPO3 to any of the configured S3 buckets with a few clicks. Supports read/write separation across multiple buckets.',
+	'category' => 'misc',
+	'author' => 'Arno Schoon',
+	'author_email' => 'arno@maxserv.com',
+	'author_company' => 'MaxServ B.V.',
+	'state' => 'beta',
+	'clearCacheOnLoad' => 0,
+	'version' => '0.0.1',
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
