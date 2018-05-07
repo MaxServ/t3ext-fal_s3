28fc7c3d120c0d3f5eb50cb236747dc91b113318
Richard Vollebregt <rvollebregt@users.noreply.github.com>
2018-05-07 14:09:35 +0200

    Add basePath when creating files
    Basepath was missing when creating files

--- Classes/Driver/AmazonS3Driver.php
+++ Classes/Driver/AmazonS3Driver.php
@@ -434,12 +434,17 @@ class AmazonS3Driver extends TYPO3\CMS\Core\Resource\Driver\AbstractHierarchical
     {
         $parentFolderIdentifier = $this->canonicalizeAndCheckFolderIdentifier($parentFolderIdentifier);
         $targetFileIdentifier = rtrim($parentFolderIdentifier, '/') . $this->canonicalizeAndCheckFileIdentifier($fileName);
+        $basePath = '';
+
+        if (array_key_exists('basePath', $this->configuration) && !empty($this->configuration['basePath'])) {
+            $basePath = '/' . trim($this->configuration['basePath'], '/');
+        }       
 
         // create an empty file using the putObject method instead of the wrapper
         // file_put_contents() without data or touch() yield unexpected results
         $this->s3Client->putObject(array(
             'Bucket' => $this->configuration['bucket'],
-            'Key' => ltrim($targetFileIdentifier, '/'),
+            'Key' => ltrim($basePath . $targetFileIdentifier, '/'),
             'Body' => ''
         ));
 
