4638b51330c64a85fc95e8452494883d3ff72859
Richard Vollebregt <richard.vollebregt@maxserv.com>
2019-01-23 12:10:01 +0100

    [TASK] add tests for getPublicURL and correct test for processConfiguration (FS-22)

--- Tests/Unit/Driver/AmazonS3DriverTest.php
+++ Tests/Unit/Driver/AmazonS3DriverTest.php
@@ -27,37 +27,11 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class AmazonS3DriverTest extends UnitTestCase
 {
     /**
-     * @var array
+     * @param $configuration
      */
-    protected $configuration;
-
-    /**
-     * Setup for tests
-     */
-    protected function setUp()
+    protected function setConfiguration($configuration)
     {
-        parent::setUp();
-
-        $this->setConfiguration();
-    }
-
-    /**
-     * Set configuration for tests
-     */
-    protected function setConfiguration()
-    {
-        $this->configuration['content'] = [
-            'basePath' => '',
-            'bucket' => 's3://web.maxserv.com.development',
-            'key' => 'S3BUCKETTESTKEY',
-            'secret' => 'S3BUCKETTESTSECRET',
-            'region' => 'eu-west-1',
-            'title' => 'Content Storage'
-        ];
-
-        $this->configuration['stream_protocol'] = 's3.' . GeneralUtility::shortMD5(
-            AmazonS3Driver::DRIVER_KEY . '.' . $this->configuration['configurationKey']
-        );
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fal_s3']['storageConfigurations'] = $configuration;
     }
 
     /**
@@ -65,12 +39,13 @@ class AmazonS3DriverTest extends UnitTestCase
      *
      * @dataProvider processConfigurationDataProvider
      */
-    public function testProcessConfigurationThrowsErrorOnInvalidConfiguration(array $config)
+    public function testProcessConfigurationThrowsErrorOnInvalidConfiguration(array $configurationKey, $configuration)
     {
         $this->expectException(InvalidConfigurationException::class);
+        $this->setConfiguration($configuration);
 
         /** @var AmazonS3Driver $driver */
-        $driver = GeneralUtility::makeInstance(AmazonS3Driver::class, [$config]);
+        $driver = GeneralUtility::makeInstance(AmazonS3Driver::class, $configurationKey);
         $driver->processConfiguration();
     }
 
@@ -81,31 +56,142 @@ class AmazonS3DriverTest extends UnitTestCase
     {
         return [
             [
-                'content' => [
-                    'bucket' => 's3://web.maxserv.com.development',
-                    'key' => 'S3BUCKETTESTKEY',
-                    'secret' => 'S3BUCKETTESTSECRET',
-                    'title' => 'Content Storage',
-                ],
-            ],
-            [
-                'content' => [
-                    'bucket' => 's3://web.maxserv.com.development',
-                    'secret' => 'S3BUCKETTESTSECRET',
-                    'region' => 'eu-west-1',
-                    'title' => 'Content Storage',
+                ['configurationKey' => 'content'],
+                [
+                    'content' => [
+                        'bucket' => 'web.maxserv.com.development',
+                        'key' => 'S3BUCKETTESTKEY',
+                        'secret' => 'S3BUCKETTESTSECRET',
+                        'title' => 'Content Storage',
+                    ],
                 ]
             ],
             [
-                'content' => [
-                    'bucket' => 's3://web.maxserv.com.development',
-                    'key' => 'S3BUCKETTESTKEY',
-                    'region' => 'eu-west-1',
-                    'title' => 'Content Storage',
+                ['configurationKey' => 'content'],
+                [
+                    'content' => [
+                        'bucket' => 'web.maxserv.com.development',
+                        'secret' => 'S3BUCKETTESTSECRET',
+                        'region' => 'eu-west-1',
+                        'title' => 'Content Storage',
+                    ]
                 ]
             ],
             [
+                ['configurationKey' => 'content'],
+                [
+                    'content' => [
+                        'bucket' => 'web.maxserv.com.development',
+                        'key' => 'S3BUCKETTESTKEY',
+                        'region' => 'eu-west-1',
+                        'title' => 'Content Storage',
+                    ]
+                ]
+            ],
+            [
+                ['configurationKey' => 'contentStorage'],
+                [
+                    'content' => [
+                        'bucket' => 'web.maxserv.com.development',
+                        'key' => 'S3BUCKETTESTKEY',
+                        'secret' => 'S3BUCKETTESTSECRET',
+                        'region' => 'eu-west-1',
+                        'title' => 'Content Storage',
+                    ]
+                ]
+            ],
+            [
+                ['configurationKey' => ''],
                 []
+            ]
+        ];
+    }
+
+    /**
+     * @param $fileName
+     * @param $expected
+     *
+     * @dataProvider getPublicUrlDataProvider
+     */
+    public function testGetPublicUrlReturnsCorrectUrlBasedOnSuppliedConfiguration($configurationKey, $configuration, $fileName, $expected)
+    {
+        $this->setConfiguration($configuration);
+
+        /** @var AmazonS3Driver $driver */
+        $driver = GeneralUtility::makeInstance(AmazonS3Driver::class, $configurationKey);
+        $driver->processConfiguration();
+
+        $publicUrl = $driver->getPublicUrl($fileName);
+
+        $this->assertEquals($expected, $publicUrl);
+    }
+
+    /**
+     * @return array
+     */
+    public function getPublicUrlDataProvider()
+    {
+        return [
+            [
+                ['configurationKey' => 'content'],
+                [
+                    'content' => [
+                        'basePath' => '',
+                        'bucket' => 'web.maxserv.com.development', // s3:// is stripped since method function does this in the FAL S3 driver
+                        'key' => 'S3BUCKETTESTKEY',
+                        'secret' => 'S3BUCKETTESTSECRET',
+                        'region' => 'eu-west-1',
+                        'title' => 'Content Storage',
+                        'publicBaseUrl' => 'https://d2umlfc4a11nkp.cloudfront.net'
+                    ]
+                ],
+                'user_upload/photo.jpg',
+                'https://d2umlfc4a11nkp.cloudfront.net/user_upload/photo.jpg'
+            ],
+            [
+                ['configurationKey' => 'content'],
+                [
+                    'content' => [
+                        'basePath' => 'fileadmin',
+                        'bucket' => 'web.maxserv.com.development', // s3:// is stripped since initialize method does this in the FAL S3 driver
+                        'key' => 'S3BUCKETTESTKEY',
+                        'secret' => 'S3BUCKETTESTSECRET',
+                        'region' => 'eu-west-1',
+                        'title' => 'Content Storage',
+                        'publicBaseUrl' => 'https://d2umlfc4a11nkp.cloudfront.net'
+                    ]
+                ],
+                'user_upload/photo.jpg',
+                'https://d2umlfc4a11nkp.cloudfront.net/fileadmin/user_upload/photo.jpg'
+            ],
+            [
+                ['configurationKey' => 'content'],
+                [
+                    'content' => [
+                        'bucket' => 'web.maxserv.com.development', // s3:// is stripped since initialize method does this in the FAL S3 driver
+                        'key' => 'S3BUCKETTESTKEY',
+                        'secret' => 'S3BUCKETTESTSECRET',
+                        'region' => 'eu-west-1',
+                        'title' => 'Content Storage',
+                    ]
+                ],
+                'user_upload/photo.jpg',
+                'https://web.maxserv.com.development.s3.amazonaws.com/user_upload/photo.jpg'
+            ],
+            [
+                ['configurationKey' => 'content'],
+                [
+                    'content' => [
+                        'basePath' => 'fileadmin',
+                        'bucket' => 'web.maxserv.com.development', // s3:// is stripped since initialize method does this in the FAL S3 driver
+                        'key' => 'S3BUCKETTESTKEY',
+                        'secret' => 'S3BUCKETTESTSECRET',
+                        'region' => 'eu-west-1',
+                        'title' => 'Content Storage',
+                    ]
+                ],
+                'user_upload/photo.jpg',
+                'https://web.maxserv.com.development.s3.amazonaws.com/fileadmin/user_upload/photo.jpg'
             ]
         ];
     }
