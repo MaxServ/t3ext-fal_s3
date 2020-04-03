a070431a602d0e4026c36f127989b6aabfafb7dc
Koen Wouters <koen.wouters@maxserv.com>
2020-04-03 09:53:57 +0200

    [TASK] Raised required and depends version to 10.4.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.9.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.12-9.5.99',
+            'typo3' => '6.2.12-10.4.99',
         ]
     ]
 ];
