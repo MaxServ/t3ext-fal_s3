f80072d81f0f4c3471611ce6f4049f6489ba1d18
Dennis Prinse <dennis.prinse@maxserv.com>
2022-06-17 14:04:26 +0200

    [TASK] Add icon registry configuration file

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    \MaxServ\FalS3\Toolbar\ToolbarItem::ITEM_ICON_IDENTIFIER => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:fal_s3/Resources/Public/Icons/FlushCache.svg'
+    ],
+];
