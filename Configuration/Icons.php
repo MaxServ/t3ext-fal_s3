fc8e562ff882ec82c43fec7458db0e087400d89b
Dennis Prinse <dennis.prinse@maxserv.com>
2023-06-27 16:41:26 +0200

    [BUGFIX] Use FlushCacheActionEvent constants instead of ToolbarItem

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    \MaxServ\FalS3\Toolbar\ToolbarItem::ITEM_ICON_IDENTIFIER => [
+    \MaxServ\FalS3\Resource\Event\FlushCacheActionEvent::ITEM_ICON_IDENTIFIER => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         'source' => 'EXT:fal_s3/Resources/Public/Icons/FlushCache.svg'
     ],
