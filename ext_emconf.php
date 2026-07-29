2a1b2fe41ce4b5678a2037f554c642b85101e035
Dennis Prinse <dennis.prinse@maxserv.com>
2026-07-29 09:10:50 +0200

    feat(typo3): add v14 support
    [T3U14-86]

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,9 +11,9 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0 - 13.4.99',
-            'backend' => '13.4.0 - 13.4.99',
-            'extbase' => '13.4.0 - 13.4.99',
+            'typo3' => '13.4.0 - 14.3.99',
+            'backend' => '13.4.0 - 14.3.99',
+            'extbase' => '13.4.0 - 14.3.99',
         ],
         'suggests' => [
             'extractor' => '2.6.2-2.99.99'
